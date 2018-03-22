04ce9b9beeff5835672a6616ce75d036facab77d
Patrick O'Dacre <patrick@patrickwho.me>
2018-03-22 16:05:31 -0400

    Add updated scripts and styles

--- public/custom-scripts.js
+++ public/custom-scripts.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var thing = [1, 2, 3];
-
-var thing2 = function thing2() {
-  console.log('loaded again');
-};
+document.addEventListener('turbolinks:load', function (evt) {
+  feather.replace();
+});
