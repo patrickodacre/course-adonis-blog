a241be433acb2484515d5615cb387dce043cf2c3
Patrick O'Dacre <patrick@patrickwho.me>
2018-03-22 15:54:38 -0400

    Add routes ctrl and views

---
+++ app/Controllers/Http/CategoryController.js
@@ -0,0 +1,83 @@
+'use strict'
+
+const Category = use('App/Models/Category')
+
+class CategoryController {
+  async index({ view, response }) {
+    const categories = await Category.all().then(data => data.toJSON())
+
+    response.header('Turbolinks-Location', '/categories')
+
+    return view.render('categories.categories', {
+      categories
+    })
+  }
+
+  async create({ view, response }) {
+    response.header('Turbolinks-Location', '/categories/add')
+
+    return view.render('categories.category-editor')
+  }
+
+  async store({ request, response }) {
+    const { name, description, cat_slug } = request.post()
+
+    const category = await Category.create({ name, description, cat_slug })
+
+    return response.redirect('/categories')
+  }
+
+  async show({ view, response, params: { slug } }) {
+    const category = await this.findCategory(slug).then(data => data.toJSON())
+    response.header('Turbolinks-Location', '/categories/' + category.slug)
+
+    return view.render('categories.category', {
+      category
+    })
+  }
+
+  async edit({ view, response, params: { slug } }) {
+    const category = await this.findCategory(slug).then(data => data.toJSON())
+    response.header('Turbolinks-Location', '/categories/edit/' + category.slug)
+
+    return view.render('categories.category-editor', {
+      category
+    })
+  }
+
+  async update({ request, response, params: { slug } }) {
+    const category = await this.findCategory(slug)
+
+    const { name, description, cat_slug } = request.post()
+
+    category.name = name || category.name
+    category.description = description || category.description
+    category.cat_slug = cat_slug || category.cat_slug
+
+    await category.save()
+
+    return response.redirect('/categories')
+  }
+
+  async destroy({ params: { slug }, response }) {
+    const category = await this.findCategory(slug)
+
+    await category.delete()
+
+    return response.redirect('/categories')
+  }
+
+  async findCategory(slug) {
+    const query = Category.query()
+
+    if (Number.isInteger(parseInt(slug, 10))) {
+      query.where('id', slug).orWhere('slug', slug)
+    } else {
+      query.where('slug', slug)
+    }
+
+    const category = await query.first()
+  }
+}
+
+module.exports = CategoryController
--- start/routes.js
+++ start/routes.js
@@ -17,3 +17,14 @@ const Route = use('Route')
 
 Route.on('/').render('index')
 Route.on('/page').render('page')
+
+/* Categories */
+Route.get('/categories', 'CategoryController.index')
+Route.get('/categories/add', 'CategoryController.create')
+Route.get('/categories/edit/:slug', 'CategoryController.edit')
+Route.get('/categories/delete/:slug', 'CategoryController.destroy')
+Route.get('/categories/:slug', 'CategoryController.show')
+
+// form handlers
+Route.post('/categories', 'CategoryController.store')
+Route.put('/categories/:slug', 'CategoryController.update')
