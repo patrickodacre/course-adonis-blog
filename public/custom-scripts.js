186f9d93481c12dc3acba358550f8abff3542f4b
Patrick O'Dacre <patrick@patrickwho.me>
2018-04-02 13:57:45 -0400

    Update scripts and styles

--- public/custom-scripts.js
+++ public/custom-scripts.js
@@ -1,5 +1,53 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Categories =
+/*#__PURE__*/
+function (_Stimulus$Controller) {
+  _inherits(Categories, _Stimulus$Controller);
+
+  function Categories() {
+    _classCallCheck(this, Categories);
+
+    return _possibleConstructorReturn(this, (Categories.__proto__ || Object.getPrototypeOf(Categories)).apply(this, arguments));
+  }
+
+  _createClass(Categories, [{
+    key: "initialize",
+    value: function initialize() {}
+  }, {
+    key: "deleteCategory",
+    value: function deleteCategory(evt) {
+      evt.preventDefault();
+      var catId = evt.currentTarget.getAttribute('data-category-id');
+      return axios.delete('http://localhost:3333/categories/' + catId).then(function (resp) {
+        location.reload();
+      });
+    }
+  }], [{
+    key: "targets",
+    get: function get() {}
+  }]);
+
+  return Categories;
+}(Stimulus.Controller);
+"use strict";
+
+var app = Stimulus.Application.start();
+app.register('categories', Categories);
 document.addEventListener('turbolinks:load', function (evt) {
   feather.replace();
 });
